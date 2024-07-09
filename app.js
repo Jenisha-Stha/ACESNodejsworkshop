96a13983c7b73185e95c28a34d1c3f8ba9cd9948
Jenisha Shrestha <1jenishastha@gmail.com>
2024-07-09 09:42:41 +0545

    js3

--- app.js
+++ app.js
@@ -1,54 +1,81 @@
 const express = require("express");
-const { connect } = require("mongoose");
+const {
+    connect
+} = require("mongoose");
 const connectToDb = require("./database/databaseConnection");
 const Blog = require("./model/blogModel");
-const app = express()   //app ma object ayera bascha
-const PORT= 3000;
+const app = express() //app ma object ayera bascha
+const PORT = 3000;
+const multer = require("./middleware/multerConfig").multer
+const storage = require("./middleware/multerConfig").storage
+const upload = multer({storage : storage})
 
 app.use(express.json())
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({
+    extended: true
+}))
 
 
-app.set('view engine',"ejs")
+app.set('view engine', "ejs")
 
-app.get("/",(req,res)=>{
-    res.send("<h1>this is about page</h1>")
+app.get("/", async (req, res) => {
+    const blogs = await Blog.find() //always returns array
+    console.log(blogs)
+    // if(blogs.length == 0){
+    //     res.send("no blogs")
+    // }
+    res.render("index.ejs",{blogs})
    
+
 })
-app.get("/about",(req,res)=>{
+app.get("/about", (req, res) => {
     const name = "jenisha shrestha"
-    res.render("about.ejs",{name})
-   
+    res.render("about.ejs", {
+        name
+    })
+
 })
-app.get("/contact",(req,res)=>{
+app.get("/contact", (req, res) => {
     const name = "Aces workshop"
-    res.render("contact.ejs",{name})
-   
+    res.render("contact.ejs", {
+        name
+    })
+
 })
-app.get("/createblog",(req,res)=>{
-   
+app.get("/createblog", (req, res) => {
+
     res.render("createblog.ejs")
-   
+
 })
 
-app.post("/createblog", async (req,res)=>{
-    //console.log(req.body)
+app.post("/createblog",upload.single('image-url'), async (req, res) => {
+    
     //const title = req.body.title
-    const {title,subtitle,content} = req.body
-    console.log(title,subtitle,content)
-      const blog= await Blog.create({
-        title ,
-        subtitle ,
-        content 
+    const fileName = req.file.filename
+    console.log(req.file)
+    const {
+        title,
+        subtitle,
+        content
+    } = req.body
+    console.log(req.body)
+   // console.log(title, subtitle, content)
+    const blog = await Blog.create({
+        title,
+        subtitle,
+        image : fileName,
+        content
+       
+         
     })
     res.send(blog)
 })
 
-
+app.use(express.static("./storage"))
 
 //function pass bhairacha as a argument in a method is callback function
 
-app.listen(PORT,() =>{
+app.listen(PORT, () => {
     connectToDb();
     console.log(`http:localhost:${PORT}`)
 })
